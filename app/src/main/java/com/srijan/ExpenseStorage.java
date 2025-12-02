77fc8e08a094342ecc387a6b11135bcf958eb0dc
srijanpusp <puspsrijan@gmail.com>
2025-12-02 17:24:22 +0530

    Implemented saveExpense and loadExpense

--- app/src/main/java/com/srijan/ExpenseStorage.java
+++ app/src/main/java/com/srijan/ExpenseStorage.java
@@ -1,5 +1,34 @@
 package com.srijan;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class ExpenseStorage {
-    
+    private static String FILENAME = "expemse.txt";
+
+    public static void saveExpense(ArrayList<Expense> expenses){
+        try(PrintWriter writer = new PrintWriter(new FileWriter(FILENAME))){
+            for (Expense e : expenses ){
+                writer.println(e.getDescription() + " , " + e.getDate() + " , " + e.getAmount());
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static ArrayList<Expense> loadExpense(){
+        ArrayList<Expense> expenses = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(FILENAME))){
+            String line;
+            while((line = reader.readLine()) != null){
+                String[] parts = line.split(",");
+                expenses.add(new Expense(new Date(Date.parse(parts[1])), parts[0].trim(), Double.parseDouble(parts[2].trim())));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return expenses;
+    }
+
 }
